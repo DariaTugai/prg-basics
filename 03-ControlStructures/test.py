79addc9cd14e0daafd2a2fa696b6cb8e93a8aa32
DariaTugai <243773@student.uek.krakow.pl>
2025-10-30 19:47:30 +0100

    other tasks

--- 03-ControlStructures/test.py
+++ 03-ControlStructures/test.py
@@ -1,13 +1,4 @@
-#print("Hello")
-#abc = 2+3
-#print(abc)
-
-age = 23
-print(age)
-print("My age is", age)
-print(f"My age is {age+15}")
-
-university = input("Your university: ")
-
-age = input("Your age:")
-age = int(age)
+city = 'Krakow'
+university = 'AGH University of Science and Technology'
+for i in range(5):
+    print(city)
