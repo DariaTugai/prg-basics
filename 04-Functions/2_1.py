042b2f758355106c77b1654b78d258ba35419485
DariaTugai <243773@student.uek.krakow.pl>
2025-11-06 19:35:41 +0100

    2_1

---
+++ 04-Functions/2_1.py
@@ -0,0 +1,31 @@
+###
+# Program for testing built-in functions
+#
+
+
+max_number = max(7,5,6,3,8,2)
+print('Max number of 7,5,6,3,8,2 is', max_number)
+
+min_number = min(4,7,2,3,9,8)
+print('Min number of 4,7,2,3,9,8 is', min_number)
+
+str_length = len("computer science")
+print('The number of characters in "computer science" is', str_length)
+
+a=input("Enter a letter: ")
+print("You entered:", a)
+
+num=int('20303')
+print("The number is:", num)
+
+binary=bin(304)
+print("The binary value of 304 is:", binary)
+
+hexadecimal=hex(304)
+print("The hexadecimal value of 304 is:", hexadecimal)
+
+unicode=ord("€")
+print("The unicode value of € is:", unicode)
+ 
+absolute_value=abs(-17)
+print("The absolute value of -17 is:", absolute_value)
