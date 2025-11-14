8ec31699b832661fc2abe08d9c3547dbc5ae7944
DariaTugai <243773@student.uek.krakow.pl>
2025-11-14 18:38:41 +0100

    Add files via upload

--- 03-ControlStructures/atm.py
+++ 03-ControlStructures/atm.py
@@ -11,7 +11,8 @@ while True:
     print("2. Deposit")
     print("3. Withdraw")
     print("4. Exit")
-
+    print("5. Check PIN")
+    print("6. Change PIN")
     choice = input("Choose an option (1-4): ")
     print()
 
@@ -31,5 +32,23 @@ while True:
     elif choice == '4':
         print("Exiting... Thank you for using the ATM!")
         break  # Exit the loop
+    elif choice == '5':
+        while True:
+            pin_entered=(input('Enter your PIN: '))
+            if pin_entered==pin:
+                print('Entered PIN is correct.')
+                break
+            else:
+                print('Incorrect. Try again.')
+    elif choice== '6':
+        is_ok= 'True'
+        while True:
+            draft_pin=input('Enter new PIN: ')
+            if len(draft_pin)==4 and draft_pin.isdigit():
+                pin=draft_pin
+                print('Your PIN has been changed.')
+                break
+            else:
+                print('Something went wrong. Please try again.')
     else:
         print("Invalid option. Please try again.")
--- 03-ControlStructures/timer.py
+++ 03-ControlStructures/timer.py
@@ -8,9 +8,31 @@ import time
 
 countdown = int(input("Enter the number of seconds to count down: "))
 
-while countdown > 0:
+while countdown > 5:
     print(countdown)
     countdown -= 1
     time.sleep(1)  # Wait for 1 second
+while countdown == 5:
+    print('five')
+    countdown -= 1
+    time.sleep(1)  # Wait for 1 second
+while countdown == 4:
+    print('four')
+    countdown -= 1
+    time.sleep(1)  # Wait for 1 second
+while countdown == 3:
+    print('three')
+    countdown -= 1
+    time.sleep(1)  # Wait for 1 second
+while countdown == 2:
+    print('two')
+    countdown -= 1
+    time.sleep(1)  # Wait for 1 second
+while countdown == 1:
+    print('one')
+    countdown -= 1
+    time.sleep(1)  # Wait for 1 second
+
+
 
 print("Time's up!")
