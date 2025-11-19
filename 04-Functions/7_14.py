02a8f2406fd9236bd610261934085dadc20cef97
DariaTugai <243773@student.uek.krakow.pl>
2025-11-20 00:19:40 +0100

    Add files via upload

---
+++ 04-Functions/7_14.py
@@ -0,0 +1,12 @@
+def f(n1,n2,operator):
+    if operator=='+':
+        return n1+n2
+    elif operator=='-':
+        return n1-n2
+    elif operator=='*':
+        return n1*n2
+    elif operator=='%':
+            return n1%n2
+    elif operator=='**':
+        return n1**n2
+print(f(5,4,'%'))
