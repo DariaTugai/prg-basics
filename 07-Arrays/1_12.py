ec214a88f74ebcb081d621e256420f16b9e10a29
DariaTug <243773@student.uek.krakow.pl>
2025-11-24 18:11:04 +0100

    2

---
+++ 07-Arrays/1_12.py
@@ -0,0 +1,4 @@
+categories = ["Food", "Transport", "Rent","Entertainment"]
+expenses = [500, 150, 1000, 200]
+maxex=expenses.index(max(expenses))
+print('The most expensive category: ',categories[maxex])
