8729673fc6dd28cca03228c0a0ee2246ea2ce736
DariaTug <243773@student.uek.krakow.pl>
2025-11-27 23:10:59 +0100

    n

---
+++ 09-DictionariesStacksAndQueues/1_3.py
@@ -0,0 +1,10 @@
+mobile = {
+"OS":"Android",
+'brand':'samsung',
+'model':'A54 5G',
+'Year of release':'2023',
+'storage capacity':'128 GB',
+'battery life':'1000 mAh'
+}
+for key,value in mobile.items():
+   print(f"{key} : {value}")
---
+++ 09-DictionariesStacksAndQueues/1_4.py
@@ -0,0 +1,11 @@
+person = {
+   "name": "Marek",
+   "surname": "Banach",
+   "age": 25,
+   "hobby": ["swimming","excursions"],
+   "married": True,
+   "phone":{"landline":"123444321","mobile":"777888999"}
+}
+for i in person.items():
+    print(i)
+    
