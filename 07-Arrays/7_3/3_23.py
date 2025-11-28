f6a21d8a6c6ae7950d104966a495c13a142ec3d9
DariaTug <243773@student.uek.krakow.pl>
2025-11-28 21:02:58 +0100

    k

--- 07-Arrays/7_3/3_23.py
+++ 07-Arrays/7_3/3_23.py
@@ -1,7 +1,21 @@
 text='An apple a day keeps the doctor away'
 def x(text):
     count=0
-    for i in text:
+    for i in text.split(' '):
         count+=1
     return count
 print(x(text))
+def g(text):
+    g=text.split(' ')
+    gg=[]
+    for i in g:
+        gg.append(i)
+    return sorted(gg,key=len,reverse=True)
+print(g(text))
+def m(text):
+    l=text.split(' ')
+    lg=[]
+    for i in l:
+        lg.append(i)
+    return sorted(lg)
+print(m(text))
