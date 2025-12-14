15229a5f098257bc8827d04bfbb65dbe1e1a88ee
DariaTug <243773@student.uek.krakow.pl>
2025-12-14 22:55:51 +0100

    ll

--- 08-FileHandling/8_4/4_6.py
+++ 08-FileHandling/8_4/4_6.py
@@ -3,7 +3,12 @@ with open(name,'r') as file:
     content=file.read().splitlines()
     numlines=len(content)
     numchar=0
+    numwor=0
     for line in content:
-        numchar+=len(line)
+        words= line.split()
+        numwor+=len(words)
+        for word in words:
+            numchar+=len(word)
 print(numlines)
 print(numchar)
+print(f'words: {numwor}')
