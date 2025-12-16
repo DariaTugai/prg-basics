8bb32933a1d2279a7a9220f04fd1c0435b97450b
DariaTug <243773@student.uek.krakow.pl>
2025-12-16 19:21:21 +0100

    j

---
+++ 09-DictionariesStacksAndQueues/9_3/3_3.py
@@ -0,0 +1,20 @@
+import queue
+
+expression1 = "[(2+3)*4+5]/6-{(7*8)+[4]}" # brackets ok
+expression2 = "[(2+3]/4)"                 # brackets not correct
+expression3 = "(2-3*4+(5/6)"              # brackets not correct
+
+def brackets_ok(expression):
+   ...
+   ...
+   ...
+   return #True if brackets in expression are ok of False otherwise
+
+if brackets_ok(expression1):
+   print(...)
+else
+   ...
+
+if brackets_ok(expression2):
+...
+...
